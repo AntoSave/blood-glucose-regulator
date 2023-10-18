16c89326855aef727e8e3c4edb5e6316dfa0aa98
AntoSave <kiraanto@hotmail.it>
2023-10-18 16:10:56 +0200

    .

---
+++ WP2/WP2.m
@@ -0,0 +1,61 @@
+%x1 concentrazione del glucosio
+%x2 concentrazione di insulina nei liquidi interstiziali 
+
+p1 = 0.0151; %tasso base di rimozione del glucosio dal sangue
+p2 = 0.0313; %tasso rimozione del glucosio dovuto all'insulina
+p3 = 0.0097;
+ge = 0.97;
+ie = 0.003;
+
+%% Linearizzazione attorno al punto di equilibrio per u=1.003
+%Punto di equilibrio per u=1.003
+[x1_eq,x2_eq] = get_equilibrium(u_eq);
+x1_eq,x2_eq
+%Linearizzazione attorno al punto di equilibrio
+A = [-p1-x2_eq -x1_eq; 0 -p2];
+B = [0; p3];
+C = [1 0];
+
+%% Progettazione v1 con azione integrale + pole placement
+A = [-p1-x2_eq -x1_eq; 0 -p2];
+B = [0; p3];
+C = [1 0];
+
+% Adesso devo verificare la raggiungibilità per il sistema esteso
+% estendiamo sia lo stato che l'ingresso per studiare la raggiungibilità
+% del nuovo sistema
+Atilde = [A [0 0].'; C 0]
+Btilde = [B [0 0].'; 0 -1]
+
+%Matrice di raggiungibilità
+WR = [Btilde Atilde*Btilde Atilde^2*Btilde];
+rank(WR) %il sistema è raggiungibile perché W_R ha rango massimo. Posso sintetizzare il controllore
+
+%Adesso dobbiamo tarare K e Kr
+K = place(Atilde, [B; 0], [-1 -2 -3]); %Approfondire perché si usa B così
+
+%% Progettazione v1 con LQR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% K = place(A,B,[-1 -2]);
+% syms kr x1 x2 r
+% eqn1 = -(p1+x2)*x1+p1*ge == 0;
+% eqn2 = -(p2*x2)+p3*(-K*[x1, x2].'+kr*r-ie) == 0;
+% eqn3 = x1 == r;
+% eqn4 = r == 0.0451;
+% solve([eqn1, eqn2, eqn3, eqn4], kr,x1,x2,r)
+% kr=-67679051051420421758406738455527521857/97697717869106025718365625318899712;
+
