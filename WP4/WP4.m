fd8915d582ca86b76facf8b3da0e38d9bf79396b
AntoSave <kiraanto@hotmail.it>
2023-11-03 10:57:32 +0100

    .

--- WP4/WP4.m
+++ WP4/WP4.m
@@ -8,7 +8,7 @@ charpoly = [1 -alpha -beta];
 %settling = 4/omega0
 %rise time = 2.7/omega0
 zita = 1;
-settling_time=1;
+settling_time=10;
 omega_n = 4.0/settling_time;
 
 des_poly = [1 2*zita*omega_n omega_n^2];
